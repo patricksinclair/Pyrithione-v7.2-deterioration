654bb384b8faad4509fc75557eca9eac60ea851d
Patrick Sinclair <patricksinclair@hotmail.co.uk>
2019-09-03 12:54:08 +0100

    k and det r with bigger tau

--- src/BioSystem.java
+++ src/BioSystem.java
@@ -31,7 +31,7 @@ class BioSystem {
         this.time_elapsed = 0.;
         this.exit_time = 0.;
         this.immigration_index = 0;
-        this.tau = 0.01;
+        this.tau = 0.2;
         this.deterioration_rate = deterioration_rate;
         this.biofilm_threshold = biofilm_threshold;
 
@@ -288,9 +288,8 @@ class BioSystem {
         double det_min = 0.002, det_max = 0.004;
         double det_increment = (det_max - det_min)/(double)n_measurements;
         double duration = 240.; //10 days
-        String filename = String.format("varying_detRate-(%.4f-%.4f)_and_thresholdK-(%.4f-%.4f)", det_min, det_max, K_min, K_max);
-        String[] headers = new String[]{"K", "det_rate", "thickness", "thick_stDev", "n_deaths", "n_detachments", "n_immigrations", "n_replications"};
-
+        String filename = String.format("varying_detRate-(%.4f-%.4f)_and_thresholdK-(%.4f-%.4f)-tau=0.2", det_min, det_max, K_min, K_max);
+        String[] headers = new String[]{"tau", "K*", "det_rate", "thickness", "thick_stDev", "n_deaths", "n_detachments", "n_immigrations", "n_replications", "n_tau_halves"};
         ArrayList<Databox> Databoxes = new ArrayList<>();
 
         for(double thresh_K = K_min; thresh_K <= K_max; thresh_K+=K_increment){
@@ -334,7 +333,7 @@ class BioSystem {
             if((bs.getTimeElapsed()%interval >= 0. && bs.getTimeElapsed()%interval <= 0.02*interval) && !alreadyRecorded){
 
                 int total_N = bs.getTotalN();
-                System.out.println("K*: "+bs.biofilm_threshold+"\td_rate: "+bs.getDeterioration_rate()+"\trep : "+i+"\tt: "+bs.getTimeElapsed()+"\tpop size: "+total_N+"\tbf_edge: "+bs.getBiofilmEdge()+"\tsystem size: "+bs.getSystemSize()+"\tc_max: "+bs.c_max);
+                System.out.println("rep : "+i+"\ttau: "+bs.tau+"\tK*: "+bs.biofilm_threshold+"\td_rate: "+bs.getDeterioration_rate()+"\tt: "+bs.getTimeElapsed()+"\tpop size: "+total_N+"\tbf_edge: "+bs.getBiofilmEdge()+"\tsystem size: "+bs.getSystemSize()+"\tc_max: "+bs.c_max);
                 alreadyRecorded = true;
             }
 
@@ -343,9 +342,9 @@ class BioSystem {
             bs.performAction();
         }
 
-        double[] counters = new double[]{bs.getN_deaths(), bs.getN_detachments(), bs.getN_immigrations(), bs.getN_replications()};
+        double[] counters = new double[]{bs.getN_deaths(), bs.getN_detachments(), bs.getN_immigrations(), bs.getN_replications(), bs.getN_tau_halves()};
 
-        return new Databox(bs.tau, bs.deterioration_rate, bs.biofilm_threshold, bs.getBiofilmThickness(), counters);
+        return new Databox(bs.tau, bs.biofilm_threshold, bs.deterioration_rate,  bs.getBiofilmThickness(), counters);
     }
 
 
--- src/PyrithioneMain.java
+++ src/PyrithioneMain.java
@@ -1,11 +1,12 @@
 public class PyrithioneMain {
     public static void main(String[] args){
 
-        //BioSystem.varyingDeteriorationAndThreshold();
+
         double scale_99 = 2.71760274, sigma_99 = 0.56002833;
         double scale_95 = 1.9246899, sigma_95 = 1.00179994;
         double scale_90 = 1.01115312, sigma_90 = 1.51378016;
 
-        BioSystem.varyingTauStep(scale_99, sigma_99);
+        BioSystem.varyingDeteriorationAndThreshold();
+        //BioSystem.varyingTauStep(scale_99, sigma_99);
     }
 }
